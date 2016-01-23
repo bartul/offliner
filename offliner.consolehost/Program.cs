9dedff1ef501d943e92b809966762c2f1713047e
Bartul Bonacic <bartul.bonacic@live.com>
2016-01-23 20:54:27 +0100

    link parsing

--- offliner.consolehost/Program.cs
+++ offliner.consolehost/Program.cs
@@ -3,24 +3,29 @@ using System.Collections.Generic;
 using System.Collections.Concurrent;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using Offliner;
 
 namespace Offliner
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
             Console.Clear();
             Console.WriteLine("Let's go!");
             
-            var progress = new Progress<DownloadProgressInfo>((p) => Console.WriteLine($"{p.Url} - Done!!!"));
+            var regexString =  "href\\s*=\\s*(?:[\"'](?<1>[^\"']*)[\"']|(?<1>\\S+))";
+            var regex = new ThreadLocal<Regex>(() => new Regex(regexString, RegexOptions.Compiled | RegexOptions.IgnoreCase));
+
+            var progress = new Progress<DownloadProgressInfo>((p) => Console.WriteLine($"{p.Page.Url} - {p.Page.HRefs.Count()} links like '{p.Page.HRefs.LastOrDefault()}' - Done!!!"));
             Task.WaitAll(
                 DownloadAsync("http://www.index.hr", progress),
                 DownloadAsync("http://www.slobodnadalmacija.hr", progress),
                 DownloadAsync("http://www.forum.tm", progress),
-                DownloadAsync("http://www.tportal.hr", progress),
+                DownloadAsync("http://www.ina.hr", progress),
                 DownloadAsync("http://www.hrt.hr", progress),
                 DownloadAsync("http://www.jutarnji.hr", progress),
                 DownloadAsync("http://www.net.hr", progress)
@@ -32,15 +37,33 @@ namespace Offliner
 
 
         public static async Task<string> DownloadAsync(string url, IProgress<DownloadProgressInfo> progress) {
-          var content = await new WebClient().DownloadStringTaskAsync(url);
-          progress.Report(new DownloadProgressInfo(url, true));
-          return content; 
-        }  
+            var regexString =  "href\\s*=\\s*(?:[\"'](?<1>[^\"']*)[\"']|(?<1>\\S+))";
+            var regex = new ThreadLocal<Regex>(() => new Regex(regexString, RegexOptions.Compiled | RegexOptions.IgnoreCase));
+
+            var content = await new WebClient().DownloadStringTaskAsync(url);
+            var hrefs = regex.Value.Matches(content).Values().Where(href => !string.IsNullOrWhiteSpace(href) && href.StartsWith(url)).ToArray();
+            
+            progress.Report(new DownloadProgressInfo(new PageInfo(url, content, hrefs), true));
+            return content; 
+        }
         public class DownloadProgressInfo
         {
-            public DownloadProgressInfo(string url, bool isDone) { Url = url; IsDone = isDone; }
-            public string Url { get; }
+            public DownloadProgressInfo(PageInfo page, bool isDone) { Page = page; IsDone = isDone; }
+            
+            public PageInfo Page { get; }
             public bool IsDone { get; }
+        }
+        public class PageInfo {
+            public PageInfo(string url, string content, string[] hrefs) { Url = url; Content = content; HRefs = hrefs; }
+            
+            public string Url { get; }
+            public string Content { get; }
+            public string[] HRefs { get; }
+        }  
+        
+        
+        public static IEnumerable<string> Values(this MatchCollection matches){
+            foreach (Match match in matches) yield return match.Groups[1].Value;
         }
     }
 }
