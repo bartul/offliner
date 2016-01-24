c0e74813f9f7e275cf1ac4008c14c51c2ff94da9
Bartul Bonacic <bartul.bonacic@live.com>
2016-01-24 17:48:32 +0100

    page grabber to encapsulate grabbing

---
+++ offliner.consolehost/PageGrabber.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Net;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Offliner;
+
+namespace Offliner 
+{
+    public class PageGrabber
+    {
+
+            private readonly static string regexString =  "href\\s*=\\s*(?:[\"'](?<1>[^\"']*)[\"']|(?<1>\\S+))";
+            private readonly static ThreadLocal<Regex> _Regex = new ThreadLocal<Regex>(() => new Regex(regexString, RegexOptions.Compiled | RegexOptions.IgnoreCase));
+
+            public async Task<PageInfo> GrabAsync(string url, IProgress<GrebberProgressInfo> progress) 
+            {
+                var content = await new WebClient().DownloadStringTaskAsync(url);
+                var hrefs = _Regex.Value.Matches(content).Values()
+                    .Where(href => !string.IsNullOrWhiteSpace(href) && href.StartsWith(url) && href.EndsWith("/", StringComparison.OrdinalIgnoreCase))
+                    .Distinct()
+                    .ToArray();
+                
+                var page = new PageInfo(url, content, hrefs);
+                progress.Report(new GrebberProgressInfo(page, true));
+                return page; 
+            }
+            public async Task<PageInfo[]> GrabAsync(string[] urls, IProgress<GrebberProgressInfo> progress) 
+            {
+                var pages = from url in urls
+                            select this.GrabAsync(url, progress);    
+                return await Task.WhenAll<PageInfo>(pages); 
+            }
+
+    }
+    public class GrebberProgressInfo
+    {
+        public GrebberProgressInfo(PageInfo page, bool isDone) { Page = page; IsDone = isDone; }
+        
+        public PageInfo Page { get; }
+        public bool IsDone { get; }
+    }
+    public class PageInfo 
+    {
+        public PageInfo(string url, string content, string[] hrefs) { Url = url; Content = content; HRefs = hrefs; }
+        
+        public string Url { get; }
+        public string Content { get; }
+        public string[] HRefs { get; }
+    }  
+    
+    public static class PageGrabberExtensions
+    {
+        public static IEnumerable<string> Values(this MatchCollection matches)
+        {
+            foreach (Match match in matches) yield return match.Groups[1].Value;
+        }
+    }
+}   
--- offliner.consolehost/Program.cs
+++ offliner.consolehost/Program.cs
@@ -17,53 +17,18 @@ namespace Offliner
             Console.Clear();
             Console.WriteLine("Let's go!");
             
-            var regexString =  "href\\s*=\\s*(?:[\"'](?<1>[^\"']*)[\"']|(?<1>\\S+))";
-            var regex = new ThreadLocal<Regex>(() => new Regex(regexString, RegexOptions.Compiled | RegexOptions.IgnoreCase));
 
-            var progress = new Progress<DownloadProgressInfo>((p) => Console.WriteLine($"{p.Page.Url} - {p.Page.HRefs.Count()} links like '{p.Page.HRefs.LastOrDefault()}' - Done!!!"));
-            Task.WaitAll(
-                DownloadAsync("http://www.index.hr", progress),
-                DownloadAsync("http://www.slobodnadalmacija.hr", progress),
-                DownloadAsync("http://www.forum.tm", progress),
-                DownloadAsync("http://www.ina.hr", progress),
-                DownloadAsync("http://www.hrt.hr", progress),
-                DownloadAsync("http://www.jutarnji.hr", progress),
-                DownloadAsync("http://www.net.hr", progress)
-            );
+            var progress = new Progress<GrebberProgressInfo>((p) => Console.WriteLine($"{p.Page.Url} - {p.Page.HRefs.Count()} links like '{p.Page.HRefs.LastOrDefault()}' - Done!!!"));
+            var grebber = new PageGrabber();            
+            
+            var firstLevelUrl = "http://www.vergic.com";
+            
+            var x = grebber.GrabAsync(firstLevelUrl, progress).Result;
+            
+            foreach (var url in x.HRefs) Console.WriteLine($"Link {url}.");
             
             Console.WriteLine("Press any key...");
             Console.Read();
-        }
-
-
-        public static async Task<string> DownloadAsync(string url, IProgress<DownloadProgressInfo> progress) {
-            var regexString =  "href\\s*=\\s*(?:[\"'](?<1>[^\"']*)[\"']|(?<1>\\S+))";
-            var regex = new ThreadLocal<Regex>(() => new Regex(regexString, RegexOptions.Compiled | RegexOptions.IgnoreCase));
-
-            var content = await new WebClient().DownloadStringTaskAsync(url);
-            var hrefs = regex.Value.Matches(content).Values().Where(href => !string.IsNullOrWhiteSpace(href) && href.StartsWith(url)).ToArray();
-            
-            progress.Report(new DownloadProgressInfo(new PageInfo(url, content, hrefs), true));
-            return content; 
-        }
-        public class DownloadProgressInfo
-        {
-            public DownloadProgressInfo(PageInfo page, bool isDone) { Page = page; IsDone = isDone; }
-            
-            public PageInfo Page { get; }
-            public bool IsDone { get; }
-        }
-        public class PageInfo {
-            public PageInfo(string url, string content, string[] hrefs) { Url = url; Content = content; HRefs = hrefs; }
-            
-            public string Url { get; }
-            public string Content { get; }
-            public string[] HRefs { get; }
-        }  
-        
-        
-        public static IEnumerable<string> Values(this MatchCollection matches){
-            foreach (Match match in matches) yield return match.Groups[1].Value;
         }
     }
 }
